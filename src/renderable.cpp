31e7412c9e3bf4bb62c2ee241b42215e130c51b2
Shaun Reich <sreich@kde.org>
2012-11-07 15:40:33 -0500

    woot! debug rendering available for all textures now
    a green rect displays around the textures' pos and size...and it's
    hashed diagonally with lines yay  :)

--- src/renderable.cpp
+++ src/renderable.cpp
@@ -22,6 +22,7 @@
 #include <SFML/System/Vector2.hpp>
 
 #include <SFML/Graphics/RectangleShape.hpp>
+#include <SFML/Graphics/VertexArray.hpp>
 
 Renderable::Renderable(const char* texture)
 {
@@ -36,18 +37,32 @@ void Renderable::setTexture(const char* texture)
     if (DEBUG_RENDERING) {
         // make things transparent so we can see, also tint them magenta
         sf::Color color(255, 0, 255, 180);
-        sf::Sprite::setColor(color);
+//        sf::Sprite::setColor(color);
     }
 }
 
 void Renderable::render(sf::RenderWindow* window)
 {
-    sf::RectangleShape outline;
     sf::Vector2u size = sf::Sprite::getTexture()->getSize();
-    outline.setSize(sf::Vector2f(size.x, size.y));
-    outline.setOutlineColor(sf::Color::Cyan);
-    outline.setPosition(sf::Sprite::getPosition());
-    outline.setFillColor(sf::Color(255, 0, 0, 255));
+    sf::Vector2f rectSize(size.x, size.y);
+    sf::Vector2f pos = sf::Sprite::getPosition();
+
+    sf::RectangleShape outline;
+    outline.setSize(rectSize);
+    outline.setOutlineColor(sf::Color::Green);
+    outline.setOutlineThickness(1);
+    outline.setPosition(pos);
+    outline.setFillColor(sf::Color(0, 0, 0, 0));
+
+    sf::VertexArray line1(sf::LinesStrip, 2);
+    line1[0].position = sf::Vector2f(pos.x, pos.y);
+    line1[1].position = sf::Vector2f(pos.x + rectSize.x, pos.y + rectSize.y);
+
+    sf::VertexArray line2(sf::LinesStrip, 2);
+    line2[0].position = sf::Vector2f(pos.x + rectSize.x, pos.y);
+    line2[1].position = sf::Vector2f(pos.x, pos.y + rectSize.y);
 
     window->draw(outline);
+    window->draw(line1);
+    window->draw(line2);
 }
