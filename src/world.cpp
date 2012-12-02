b3436995244e340bde4138884f242a1aa1f7c332
Shaun Reich <sreich@kde.org>
2012-12-01 21:33:49 -0500

    fix kb a bit

--- src/world.cpp
+++ src/world.cpp
@@ -172,16 +172,16 @@ void World::handleEvent(const sf::Event& event)
     switch (event.type) {
     case sf::Event::KeyPressed:
         if (event.key.code == sf::Keyboard::D || event.key.code == sf::Keyboard::Right) {
-            m_inputXDirection += 1.f;
+            m_inputXDirection = 1.f;
         }
         if (event.key.code == sf::Keyboard::A || event.key.code == sf::Keyboard::Left) {
-            m_inputXDirection -= 1.f;
+            m_inputXDirection = -1.f;
         }
         if (event.key.code == sf::Keyboard::S || event.key.code == sf::Keyboard::Down) {
-            m_inputYDirection += 1.f;
+            m_inputYDirection = 1.f;
         }
         if (event.key.code == sf::Keyboard::W || event.key.code == sf::Keyboard::Up) {
-            m_inputYDirection -= 1.f;
+            m_inputYDirection = -1.f;
         }
         break;
 
@@ -199,6 +199,7 @@ void World::handleEvent(const sf::Event& event)
             m_inputYDirection = 0.f;
         }
         break;
+
     case sf::Event::MouseButtonPressed:
         performBlockAttack();
         break;
