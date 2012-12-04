fec6367cd327fb575653bb25f57aa47cd9b26a6d
Shaun Reich <sreich@kde.org>
2012-12-04 18:23:23 -0500

    add cloudsystem, prepare for refactor with Sky being the container

---
+++ src/sky.h
@@ -0,0 +1,45 @@
+/******************************************************************************
+ *   Copyright (C) 2012 by Shaun Reich <sreich@kde.org>                       *
+ *                                                                            *
+ *   This program is free software; you can redistribute it and/or            *
+ *   modify it under the terms of the GNU General Public License as           *
+ *   published by the Free Software Foundation; either version 2 of           *
+ *   the License, or (at your option) any later version.                      *
+ *                                                                            *
+ *   This program is distributed in the hope that it will be useful,          *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
+ *   GNU General Public License for more details.                             *
+ *                                                                            *
+ *   You should have received a copy of the GNU General Public License        *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.    *
+ *****************************************************************************/
+
+#ifndef SKY_H
+#define SKY_H
+
+#include "renderable.h"
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Graphics/RenderStates.hpp>
+
+class CloudSystem;
+
+class Sky
+{
+public:
+    Sky(sf::RenderWindow *window, sf::View *view, float height);
+
+    void update();
+    void render();
+
+private:
+    sf::RenderWindow *m_window;
+    sf::View *m_view;
+    CloudSystem *m_cloudSystem = nullptr;
+
+    // the height of the sky
+    float m_height;
+};
+
+#endif
